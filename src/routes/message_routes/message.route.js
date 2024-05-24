2fc9ba765a208e62d182eebc04072c32c2cc3e2f
SocyGen EduTech Private Limited <115183627+Socygen@users.noreply.github.com>
2024-05-24 16:04:44 +0530

    Update message.route.js

--- src/routes/message_routes/message.route.js
+++ src/routes/message_routes/message.route.js
@@ -4,6 +4,7 @@ const message_controller = require('./message.controller');
 const auth = require('../../middleware/auth');
 
 router.post('/sendMessage',auth, message_controller.sendMessage);
+router.post('/sendgroupMessage', auth, message_controller.sendGroupMessage);
 router.get('/myMessages',auth, message_controller.myMessages);
 
 module.exports = router;
