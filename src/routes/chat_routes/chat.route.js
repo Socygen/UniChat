b562ec1944e88f561ecdf0edfb7d5795d8c73fa1
SocyGen EduTech Private Limited <115183627+Socygen@users.noreply.github.com>
2024-05-27 14:33:07 +0530

    Update chat.route.js

--- src/routes/chat_routes/chat.route.js
+++ src/routes/chat_routes/chat.route.js
@@ -9,5 +9,6 @@ router.post('/createGroupChat',auth, chat_controller.createGroupChat);
 router.post('/myChats',auth, chat_controller.myChats);
 router.get('/chatById',auth, chat_controller.chatById);
 router.post('/removegroupUser', chat_controller.removeUsersFromGroupChat);
+router.post('/addUsersToGroupChat', chat_controller.addUsersToGroupChat);
 
 module.exports = router
