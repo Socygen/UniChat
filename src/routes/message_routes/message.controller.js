b1a2fd101d6883c59e20f3618626a32b6503cbc3
SocyGen EduTech Private Limited <115183627+Socygen@users.noreply.github.com>
2024-05-29 21:31:55 +0530

    Update message.controller.js

--- src/routes/message_routes/message.controller.js
+++ src/routes/message_routes/message.controller.js
@@ -126,7 +126,7 @@ const sendGroupMessage = async (req, res) => {
 const sendNotification = async (notificationData) => {
     try {
       let findUser = await UserModel.findById(notificationData?.senderId);
-      let anotherUser = await UserModel.findById(notificationData?.receiverId);
+      let anotherUser = await UserModel.findById(notificationData?.senderId);
   
       if (!!findUser?.fcmToken) {
   
